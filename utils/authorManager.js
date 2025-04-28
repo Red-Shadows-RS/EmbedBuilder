086c9629d803bb99983eb74d6e388c4986579cb2
NightcoreAT <oussamakatalonya1@gmail.com>
2025-04-28 19:59:47 +0100

    ✨| UPLOADED

---
+++ utils/authorManager.js
@@ -0,0 +1,8 @@
+// By nightcoreat
+
+function getAuthor() {
+  return {
+    name: document.getElementById("authorNameInput").value,
+    icon_url: document.getElementById("authorIconInput").value,
+  };
+}
